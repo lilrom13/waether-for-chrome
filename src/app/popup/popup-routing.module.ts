0776848d6fed790130c15683cf2106365af450bf
Margheriti <lilrom13@gmail.com>
2017-02-09 19:58:50 -0500

    create popup route

--- src/app/popup/popup-routing.module.ts
+++ src/app/popup/popup-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PopupComponent } from "./popup.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PopupComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
